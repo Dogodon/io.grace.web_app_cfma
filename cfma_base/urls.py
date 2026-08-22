307b748c2db89b5ead845c71df7737976859e95b
Dtmdp@.c7 <dogodontraore@gmail.com>
2026-08-22 11:45:55 +0000

    Fix: Création du fichier urls.py manquant dans cfma_base

---
+++ cfma_base/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # C'est ici que vous ajouterez vos futures pages.
+    # Exemple : path('', views.ma_vue, name='accueil'),
+]
+
