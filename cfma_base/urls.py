67ae6d7493f316fa7021ceebcafb3865e955a542
Dtmdp@.c7 <dogodontraore@gmail.com>
2026-08-24 07:26:38 +0000

    Ajout du sitemap dynamique

--- cfma_base/urls.py
+++ cfma_base/urls.py
@@ -45,10 +45,12 @@ sitemaps = {
 
 urlpatterns = [
     # Page d'accueil de l'application
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+
     path('', views.home, name='home'), 
     
 
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    #path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 
 
     # Fil de commentaires global autonome (sans clé étrangère)
