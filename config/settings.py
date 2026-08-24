2f5e1a51ac46db98f974212931b2ccf683109a4c
Dtmdp@.c7 <dogodontraore@gmail.com>
2026-08-24 15:45:03 +0000

    ch : App mise en français

--- config/settings.py
+++ config/settings.py
@@ -417,7 +417,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
 
 TIME_ZONE = 'UTC'
 
